bfbfcc815c952afb73988667e73fae256b101337
Felipe Rivas <fe.rivasb@gmail.com>
2022-04-19 20:38:08 -0500

    task 1 changed

--- 0x0A-configuration_management/1-install_a_package.pp
+++ 0x0A-configuration_management/1-install_a_package.pp
@@ -1,6 +1,5 @@
 #Installs puppet-lint
-package { 'puppet-lint':
-  ensure   => '2.5.0',
-  source   => 'https://rubygems.org',
-  provider => 'gem'
+package { 'flask':
+  ensure   => '2.1.0',
+  provider => 'pip3',
 }
