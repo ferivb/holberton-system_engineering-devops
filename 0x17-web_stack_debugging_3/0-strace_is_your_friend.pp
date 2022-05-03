b030ba8dd3bd2c22f3f612de2536ea364a146f66
Felipe Rivas <fe.rivasb@gmail.com>
2022-05-03 10:29:42 -0500

    task 0 puppet lint correction

--- 0x17-web_stack_debugging_3/0-strace_is_your_friend.pp
+++ 0x17-web_stack_debugging_3/0-strace_is_your_friend.pp
@@ -1,4 +1,4 @@
 exec { 'correct typo':
-    command     => 'sed -i "s/phpp/php/" /var/www/html/wp-settings.php',
-    path        => '/bin/',
+    command => 'sed -i "s/phpp/php/" /var/www/html/wp-settings.php',
+    path    => '/bin/',
 }
