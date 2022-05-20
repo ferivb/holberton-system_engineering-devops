159e2d1c7b957e65b17e5a03aa5e7d50524201e3
Felipe Rivas <fe.rivasb@gmail.com>
2022-05-20 18:58:15 -0500

    task 0 pycodestyle corrections

--- 0x15-api/0-gather_data_from_an_API.py
+++ 0x15-api/0-gather_data_from_an_API.py
@@ -1,16 +1,16 @@
 #!/usr/bin/python3
 """Python script that, using a REST API, for a given
-    employee ID, returns information about 
+    employee ID, returns information about
     his/her TODO list progress."""
 
 import requests
 from sys import argv
 
 if __name__ == "__main__":
-    
     payload = {"userId": argv[1]}
-    users = requests.get('https://jsonplaceholder.typicode.com/users/{}'.format(argv[1])).json()
-    todos = requests.get('https://jsonplaceholder.typicode.com/todos/', params=payload).json()
+    url = "https://jsonplaceholder.typicode.com/"
+    users = requests.get('{}users/{}'.format(url, argv[1])).json()
+    todos = requests.get('{}todos/'.format(url), params=payload).json()
 
     total = len(todos)
     completed = 0
@@ -18,10 +18,11 @@ if __name__ == "__main__":
 
     for i in todos:
         if i.get('completed') is True:
-            completed += 1
+            done += 1
             tasks.append(i.get('title'))
 
-    print("Employee {} is done with ({}/{}).".format(users.get('name'), completed, total))
-    
+    print("Employee {} is done with ({}/{}).".format(
+        users.get('name'), done, total))
+
     for task in tasks:
         print("\t{}".format(task))
