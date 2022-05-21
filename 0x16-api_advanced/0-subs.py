2869c65183166755e9c4fdf6360a109504e0bb79
Felipe Rivas <fe.rivasb@gmail.com>
2022-05-21 18:43:35 -0500

    pyco correction

--- 0x16-api_advanced/0-subs.py
+++ 0x16-api_advanced/0-subs.py
@@ -5,21 +5,22 @@
     the function should return 0."""
 import requests
 
+
 def number_of_subscribers(subreddit):
     """Returns the number of subscribers or 0 if
         invalid subreddit"""
-    
-    # private and public keys provided by reddit
-    # CLIENT_ID = 'q5kLCDMoyRPHdgqp93f8YA'
-    # SECRET_KEY = 'tCS6kr5BHJB1Hf0LOiQ_KYAGOjRaBA'
 
     # Payload for the GET request
     url = f'https://www.reddit.com/r/{subreddit}/about.json'
     headers = {'user-Agent': 'Noobdevsayswhat'}
 
+    # Request placeholder
     res = requests.get(url, headers=headers)
 
+    # Restrain for unexistant subs
     if res.status_code > 299 and res.status_code < 500:
         return 0
+
+    # Return
     result = res.json().get('data')
     return result.get('subscribers')
