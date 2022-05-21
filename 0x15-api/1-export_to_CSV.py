b27aea166ab256db55eed15119515fb7a20f98ec
Felipe Rivas <fe.rivasb@gmail.com>
2022-05-20 21:38:26 -0500

    task 1 attempt 897689

--- 0x15-api/1-export_to_CSV.py
+++ 0x15-api/1-export_to_CSV.py
@@ -1,5 +1,6 @@
 #!/usr/bin/python3
-""" Python script to export data in the CSV format."""
+"""Python script to export data in the CSV format.
+    What the fuck is wrong with this shit"""
 import csv
 import requests
 from sys import argv
